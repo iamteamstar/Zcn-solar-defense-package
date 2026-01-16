f2ee4e144da40cb826517c137dcc514eeceb0f4d
iamteamstar <azadkocak29@gmail.com>
2026-01-16 15:25:33 +0300

    defense and aboutus pages localizer

--- ZCNGroup/Resources/Views/Defense/Index.Designer.cs
+++ ZCNGroup/Resources/Views/Defense/Index.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ZCNGroup.Resources.Views.About {
+namespace ZCNGroup.Resources.Views.Defense {
     using System;
     
     
@@ -22,14 +22,14 @@ namespace ZCNGroup.Resources.Views.About {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class About {
+    public class Index {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal About() {
+        internal Index() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace ZCNGroup.Resources.Views.About {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ZCNGroup.Resources.Views.About.About", typeof(About).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ZCNGroup.Resources.Views.Defense.Index", typeof(Index).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
